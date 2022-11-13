b96caa697866a8ff0d101859e2066ba9ca48a5fb
khanh vu <khanhvu2133@gmail.com>
2022-11-13 18:20:29 +0700

    video 12

--- src/App.js
+++ src/App.js
@@ -2,23 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import MyComponent from './component/MyComponent'
+import react from 'react'
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          khánh vũ
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
-    </div>
-  );
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     // <div>
+//     //   khánh vũ 
+//     //   <MyComponent> </MyComponent>
+//     // </div>
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           khánh vũ
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends react.Component {
+  state = {
+    name: '10',
+    address: "ninh bình",
+    age: 19
+
+  }
+  render() {
+    return (
+      <div>
+        khánh vũ
+        <MyComponent> </MyComponent>
+        Họ và tên : {this.state.name} \n
+        tuổi : {this.state.age}
+
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/component/MyComponent.js
@@ -0,0 +1,13 @@
+// class component 
+// function component
+
+import React from "react";
+class MyComponent extends React.Component {
+    // jsx 
+    render() {
+        return (
+            <div> vũ ngọc khánh</div>
+        );
+    }
+}
+export default MyComponent;
